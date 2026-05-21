ecdf2a63b08be8246b409789227a6ed90eb4f51e
Om Mishra <om.mishraa15@gmail.com>
2026-05-22 02:08:00 +0530

    Linked Github to the button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,10 +18,14 @@ const Navbar = () => {
             <a className="hover:font-bold" href="#">
               About
             </a> */}
-            <button className="flex justify-center items-center rounded-full bg-green-300 px-3 py-1 gap-1 hover:font-bold">
+            <a href="https://github.com/OmMishra15/PassMan---Your-Password-Manager-" target="_blank">
+            <button 
+            className="flex justify-center items-center rounded-full bg-green-300 px-3 py-1 gap-1 hover:font-bold">
               <img src="/icons/github.svg" alt="GitHub logo" />
               <span>GitHub</span>
             </button>
+            </a>
+            
           </li>
         </ul>
       </div>
